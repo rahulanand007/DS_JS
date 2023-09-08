e9b844fcf25f085d31b4b00934a7937060bc6449
Rahul Anand <rahul03061998@gmail.com>
2023-09-08 10:18:10 +0530

    heaps

--- Theory/heaps.js
+++ Theory/heaps.js
@@ -1,15 +1,38 @@
-class node{
-    constructor(value){
-        this.value = value
-        this.left = null
-        this.right = null
-    }
-}
 
-class heap{
+
+class maxBinaryHeap{
     constructor(){
-        this.root = null
+        this.values = []
     }
 
+    insert(value){
+        this.values.push(value)
+        if(this.values.length==1)return this.values
+
+        let index = this.values.length-1
+        let parentIndex = Math.floor((index-1)/2)
+        while(this.values[parentIndex]<this.values[index]){
+            if(this.values[parentIndex]>this.values[index]) break;
+
+            let temp = this.values[parentIndex]
+            this.values[parentIndex] = this.values[index]
+            this.values[index] = temp 
+            index = parentIndex
+            parentIndex = Math.floor((index-1)/2)
+        }
+        return this.values
+    }
     
 }
+
+let maxHeap = new maxBinaryHeap()
+maxHeap.insert(41)
+maxHeap.insert(39)
+maxHeap.insert(33)
+maxHeap.insert(18)
+maxHeap.insert(27)
+maxHeap.insert(12)
+maxHeap.insert(55)
+maxHeap.insert(1)
+maxHeap.insert(45)
+console.log(maxHeap)
